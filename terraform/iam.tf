4cd0fdc474ce9858fcf06476e8eece4e405aa659
HimanM <67066047+HimanM@users.noreply.github.com>
2025-12-29 08:44:33 +0530

    feat: Add AWS IAM roles and policies for ECS task execution, task runtime, and GitHub Actions OIDC integration.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -144,7 +144,10 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:himanm/DevOps-Project-11:*"
+          "token.actions.githubusercontent.com:sub" = [
+            "repo:HimanM/DevOps-Project-11:*",
+            "repo:himanm/DevOps-Project-11:*"
+          ]
         }
       }
     }]
